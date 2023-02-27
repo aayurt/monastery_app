0b679e4dff2ef17e8713ae3dcc01ff0bc0be4ef1
rujja <rujja.m@gmail.com>
2023-02-27 14:27:02 +0545

    go router updates

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:monastery_app/features/counter/presentation/bloc/counter_bloc.dart';
 import 'package:monastery_app/features/counter/presentation/pages/counter_page.dart';
 import 'package:monastery_app/features/todo/presentation/pages/todo_page.dart';
+import 'package:monastery_app/routes/routes.dart';
 
 import 'features/chat/presentation/bloc/chat_bloc.dart';
 import 'features/chat/presentation/pages/chat_page.dart';
@@ -18,25 +19,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => CounterBloc(),
-          ),
-          BlocProvider(
-            create: (context) => TodoBloc(),
-          ),
-           BlocProvider(
-            create: (context) => ChatBloc(),
-          ),
-        ],
-        child: const ChatPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CounterBloc(),
+        ),
+        BlocProvider(
+          create: (context) => TodoBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ChatBloc(),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRoutes.router,
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
       ),
     );
   }
